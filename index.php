e7869a1f8237f453d8877aa8415122c11f68f089
IaMGeorgious <iqbalabdulmajid03@gmail.com>
2024-05-07 22:36:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Recruitment OCELLUS</title>
+
+	<link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css">
+	<link rel="stylesheet" href="bootstrap4/css/custom.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+</head>
+<body style="background-image: url(bootstrap4/img/bg.png");">
+
+
+<?php 
+	include "navbarb4.php";
+ ?>
+
+<div class="container">
+	<!-- jumbotron -->
+<style>
+.jumbotron{
+  margin: 50px 50px 50px 50px !important;
+  box-shadow: 10px 10px 5px #888888;
+}
+
+h1{
+  font-family: tahoma;
+  color:#2980b9;
+  text-align: center;
+}
+hr{
+  width: 5%;
+  border: 1px solid black;
+  margin-bottom:60px;
+}
+
+p{
+  font-size: 19.5px;
+  text-align: justify;
+}
+
+.img-circle{
+  border: 4px solid #2980b9;
+  border-radius:100%;
+  margin-top: -260px;
+}
+</style>
+<div class="col-xs-12 jumbotron" style="background-color: transparent; backdrop-filter: blur(10px);">
+    <h1>Telkom Indonesia</h1>
+   <hr/>
+   <div class="row">
+     <div class="col-md-6 text-center">
+         <img src="https://www.allmystery.de/i/t251e55_2sqRkNr.png" alt="img1"/>
+     </div>
+      <div class="col-md-6 text-center">
+         <p>Edwin Aldrin, integrante de la misión Apolo 11 que llevó al humano por primera vez a la Luna, ha presentado su testimonio: “hubo algo allá afuera lo suficientemente cerca como para ser observado y… ¿qué podría ser?”. Consultado acerca de por qué no reportaron dicha visión en el momento, Aldrin explicó que los tres integrantes de la misión prefirieron callarlo para evitar que desde la Tierra los obligaran a volver o les exigieran realizar un acercamiento riesgoso. <a href="https://mx.tuhistory.com/noticias/astronautas-de-la-nasa-confirman-la-existencia-de-ovnis-0" target="_blank" class="btn btn-primary btn-block" role="button">ver nota completa</a>
+</p>
+     </div>
+   </div>
+ </div>
+
+<!-- end of jumbotron -->
+
+
+
+<!-- carousel -->
+	<style>
+		.carousel-item img{
+			width:  100%;
+			max-height: 400px;
+		}
+	</style>
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+
+		 <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img class="d-block w-100" src="images/2.jpg" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="images/4.jpg" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="images/5.jpg" alt="Second slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="images/3.jpg" alt="Third slide">
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+<!-- carousel -->
+
+<span style="height:40px;" ></span>
+
+	<!-- card group -->
+	<div class="row">
+	  <div class="col-sm-4">
+	    <div class="card">
+	      <div class="card-body">
+	        <h4 class="card-title">Visi</h4>
+	        <p class="card-text">Menjadi Perusahaan Pest Manajemen Service terbaik di Indonesia dengan memberikan nilai tambah untuk karyawan, pelanggan dan lingkungan</p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-4">
+	    <div class="card">
+	      <div class="card-body">
+	        <h4 class="card-title">Misi</h4>
+	        <p class="card-text">Ocellus akan memberikan layanan konsistensi, dan aman untuk karyawan, pelanggan dan lingkungan</p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-4">
+	    <div class="card">
+	      <div class="card-body">
+	        <h4 class="card-title">Tentang</h4>
+	        <p class="card-text">Nama perusahaan : PT. OCELLUS INDONESIA</p>
+	       	<p class="card-text">Tahun Berdiri : 14 November 2009</p>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<!-- card group -->
+
+
+
+</div>
+
+
+<?php 
+	include "footerb4.php";
+ ?>
+
+
+	<script src="bootstrap4/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+</html>
